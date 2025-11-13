7f28817a82300f063d4c4d5842b1afd067ac0bd6
AsherR4444 <il08082000@gmail.com>
2025-11-13 21:26:29 +0300

    update

--- orval.config.ts
+++ orval.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'orval'
 export default defineConfig({
   api: {
     input: {
-      target: './src/api/openapi.json',
+      target: './openapi.json',
     },
     output: {
       mode: 'split',
